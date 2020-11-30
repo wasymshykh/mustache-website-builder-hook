4fe1bcfc3cbed2623821290867ef1a44d45973c5
Wasym <46717121+wasymshykh@users.noreply.github.com>
2020-11-30 05:08:33 +0500

    html output generation

--- app/functions.php
+++ app/functions.php
@@ -13,7 +13,7 @@ function end_response ($status_code, $message)
     die();
 }
 
-function validated_template($company)
+function validated_template ($company)
 {
     if (!property_exists($company, 'template')) {
         return ['status' => false, 'message' => 'template is not sent!'];
@@ -25,7 +25,56 @@ function validated_template($company)
         return ['status' => false, 'message' => 'template does not exists'];
     }
 
-    return ['status' => true, 'name' => $template_name];
+    return ['status' => true, 'name' => $template_name, 'path' => $template_path];
+}
 
+function get_template_files (string $directory)
+{
+    $directories = ['/'];
+    $files = [];
+    $ignore = ['.', '..'];
+
+    while (!empty($directories)) {
+        $_dir = array_pop($directories);
+        $file_names = scandir($directory . $_dir);        
+        
+        foreach ($file_names as $file_name) {
+            if (!in_array($file_name, $ignore)) {
+                $file_path = $directory . $_dir  . $file_name;
+                
+                // check if file is an actual directory
+                if (filetype($file_path) === 'dir') {
+                    array_push($directories, $_dir . $file_name . '/');
+                }
+                else if (pathinfo($file_path)['extension'] === 'html') {
+                    // if file has .html extention
+                    if (array_key_exists($_dir, $files)) {
+                        array_push($files[$_dir], $file_name);
+                    } else {
+                        $files[$_dir] = [ $file_name ];
+                    }
+                }
+            }
+        }
+    }
+    
+    return $files;
+}
+
+
+function build_html_file($directory, $file, $data, $mustache)
+{
+    $file_name = $file;
+    if ($directory !== '/') {
+        $file_name = $directory . '/' . $file;
+    }
+    
+    $html_content = $mustache->loadTemplate(str_replace ('.html', '', $file_name))->render($data);
+
+    $output_directory = DIR . '/output/' . $directory;
+    if (!is_dir($output_directory)) {
+        mkdir($output_directory);
+    }
+    file_put_contents(DIR . '/output/' . $file_name, $html_content);
 }
 
--- index.php
+++ index.php
@@ -19,13 +19,24 @@ if (!$template['status']) {
 $m = new Mustache_Engine([
     'entity_flags' => ENT_QUOTES,
     'escape' => function($value) { return htmlspecialchars($value, ENT_COMPAT, 'UTF-8');},
-    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/templates/'.$template['name'].'/', ['extension' => '.html'])
+    'loader' => new Mustache_Loader_FilesystemLoader($template['path'], ['extension' => '.html'])
     ]);
 
 
 // check for files with r.s.t data
+$directories = get_template_files($template['path']);
 
+foreach ($directories as $directory => $files) {
+    foreach ($files as $file) {
+        
+        $data_to_pass = $data;
+        if ($directory !== '/') {
+            $directory = str_replace('/', '', $directory);
+            if (array_key_exists($directory, $data->company->category[0])) {                
+                $data_to_pass = $data->company->category[0]->$directory;
+            }
+        }
 
-echo $m->loadTemplate('home')->render(['company' => $data->company]);
-
-
+        build_html_file($directory, $file, $data_to_pass, $m);
+    }
+}
